234ead4b88084154cb82471ee3943d3e991cee91
ashish <ashishgaur407@gmail.com>
2025-10-30 11:57:14 +0530

    git update

---
+++ src/GitConcept.java
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+// Main Class should Always write......
+public class GitConcept {
+    public  static  void main(String [] args){
+        System.out.println("ashish");
+        System.out.println("hello");
+    }
+
+
+
+}
